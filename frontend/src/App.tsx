f9388a8fe9d81f18195692d4e63cb6041cb23dbc
Astin <akjolsatinov01@gmail.com>
2021-05-11 19:40:36 +0600

    add base template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import PrimaryTemplate from "./components/PrimaryTemplate";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <PrimaryTemplate />
+    </BrowserRouter>
+  );
+}
+
+export default App;
